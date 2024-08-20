8280d6a6035a5836152f75b78b7a6d1d582099cd
Junkang Liu <545757461@qq.com>
2024-08-20 16:34:38 +0200

    1

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,8 +10,8 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
+  siteTitle: '🐛',
+  siteUrl: 'https://liuBigBrother.run',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
